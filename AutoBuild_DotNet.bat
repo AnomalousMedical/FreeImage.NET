8c9e029b5ae11ced2770c9d020d1e70e3a07a78c
Andrew Piper <threax@users.noreply.github.com>
2014-12-24 16:40:28 +0000

    Added auto build for all .net libraries.

--- AutoBuild_DotNet.bat
+++ AutoBuild_DotNet.bat
@@ -8,5 +8,5 @@ set BuildCommand="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe"
 
 set SolutionPath=%ThisFolder%%SolutionName%
 
-%BuildCommand% "%SolutionPath%" /property:Configuration=Release /target:Clean
-%BuildCommand% "%SolutionPath%" /property:Configuration=Release
+%BuildCommand% "%SolutionPath%" /property:Configuration=Release;Platform="Any CPU" /target:Clean
+%BuildCommand% "%SolutionPath%" /property:Configuration=Release;Platform="Any CPU"
