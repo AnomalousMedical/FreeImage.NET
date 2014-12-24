d7940d62613aee65e8e3b2a893979f9b3961e9fe
AndrewPiper <AndrewPiper@localhost>
2014-12-24 17:00:09 +0000

    Grabbed current directory and used that to cd back to in auto build scripts.

--- AutoBuild_DotNet.bat
+++ AutoBuild_DotNet.bat
@@ -5,6 +5,7 @@ set SolutionName=FreeImage.NET.sln
 set ThisFolder=%~dp0
 set RootDependencyFolder=%ThisFolder%..\
 set BuildCommand="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe"
+set CurrentDirectory=%CD%
 
 set SolutionPath=%ThisFolder%%SolutionName%
 
